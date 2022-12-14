0999090b8cc11e366d773b490d926f09fe7d344d
dlcksgml123123 <qwedcxza0501@naver.com>
2022-12-14 09:56:48 +0900

    첫 번째 프로젝트 파일 업로드

---
+++ src/main/java/com/leech/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package com.leech.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
